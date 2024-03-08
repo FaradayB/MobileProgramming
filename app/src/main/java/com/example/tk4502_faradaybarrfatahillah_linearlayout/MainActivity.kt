79e287ffb381509498bbefb598f2752e38e26e8b
Faraday Barr <faradaybarrf@gmail.com>
2024-03-08 22:00:02 +0700

    Added Files

---
+++ app/src/main/java/com/example/tk4502_faradaybarrfatahillah_linearlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tk4502_faradaybarrfatahillah_linearlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
